a5e808828caaeb0611bd78939b79c178b904cb70
badpolymer <roans_policy.07@icloud.com>
2022-07-19 23:31:28 +0800

    Change id type of subCategory to UUID

--- DatabaseAG/Model/SubCategory.swift
+++ DatabaseAG/Model/SubCategory.swift
@@ -9,7 +9,7 @@ import Foundation
 import RealmSwift
 
 class SubCategory : Object, Identifiable {
-    @Persisted var id = ObjectId()
+    @Persisted var id = UUID()
     @Persisted var name : String = "Not defined subCat"
     @Persisted(originProperty: "subCategories") var mainCategory: LinkingObjects<MainCategory>
     @Persisted var items : List<MainItem>
