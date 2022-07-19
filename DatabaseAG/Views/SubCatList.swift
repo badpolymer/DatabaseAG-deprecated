bcaf3e03f18f35d6104059a50cc35f2389a26296
badpolymer <roans_policy.07@icloud.com>
2022-07-19 23:38:08 +0800

    Modify SubCat List

--- DatabaseAG/Views/SubCatList.swift
+++ DatabaseAG/Views/SubCatList.swift
@@ -6,39 +6,109 @@
 //
 
 import SwiftUI
-
+import RealmSwift
 
 struct SubCatList: View {
     @EnvironmentObject var controller : MainController
+    @State private var selectedSubCat : UUID?
+    @State private var editorName: String = ""
+    @State private var notModifying: Bool = true
+    
     var body: some View {
-        VStack {
-            List{
+        HStack(spacing: 5.0) {
+            
+            //Left
+            VStack {
                 if let subCategories = controller.selectedMainCat?.subCategories {
-                    
-                    
-                    ForEach(subCategories) { subCategory in
-                        SubCatRow(subCategory: subCategory)
-                            .frame(height: 10)
+                    if subCategories.count > 0 {
+                        List(subCategories,selection:$selectedSubCat){
+                            Text($0.name)
+                        }
+                        
+                        .onChange(of: selectedSubCat, perform: { newValue in
+                            controller.selectedSubCat = controller.convert(newValue)
+                        })
+                        .disabled(controller.subCategoryIsEditing)
+                    } else {
+                        List(){
+                        Text("No category yet.")
+                        }
                     }
-                    
                 }
-            }.background()
-            
-            
-            HStack(spacing: 5.0) {
-                Spacer()
-                Button("-") {
-                    print("sdsds")
-                }.frame(width: 25)
-                Button("+") {
-                    print("sdsds")
-                }.frame(width: 25)
-                Spacer()
-                    .frame(width: 5)
+                
+                
+                HStack(spacing: 5.0) {
+                    Text(controller.selectedMainCat?.name ?? "Not selected")
+                    Spacer()
+                    Button("Edit") {
+                        if selectedSubCat != nil && controller.selectedSubCat != nil {
+                            controller.subCategoryIsEditing = true
+                            editorName = controller.selectedSubCat!.name
+                            notModifying = false
+                        } else {
+                            controller.categoryEditingError("You didn't select an item.")
+                        }
+                        
+                    }.frame(width: 50)
+                    Button("-") {
+                        if let subCatToDelete = controller.selectedSubCat {
+                        controller.delete(subCatToDelete)
+                        } else {
+                            controller.categoryEditingError("You didn't select an item.")
+                        }
+                    }.frame(width: 25)
+                    Button("+") {
+                        controller.subCategoryIsEditing = true
+                        notModifying = true
+                        controller.testfunc()
+                    }.frame(width: 25)
+                    Spacer()
+                        .frame(width: 5)
+                }
             }
             
-        }
-    }
+            //right
+            if controller.subCategoryIsEditing {
+                VStack(alignment: .leading){
+                    Text("Main: \(self.controller.selectedMainCat?.name ?? "Error")")
+                    HStack {
+                        Text("Name:")
+                        TextField("Enter the name.", text: $editorName)
+                    }
+                    HStack{
+                        Spacer()
+                        Button {
+                            controller.subCategoryIsEditing = false
+                        } label: {
+                            Text("Cancel")
+                        }
+                        Button {
+                            if editorName.isEmpty {
+                                controller.categoryEditingError("Please enter a name.")
+                            } else {
+                                if notModifying {
+                                    controller.add(editorName)
+                                } else {
+                                    controller.modify(editorName)
+                                }
+                                controller.subCategoryIsEditing = false
+                                editorName = ""
+                            }
+                        } label: {
+                            Text("Save")
+                        }
+                        
+                        
+                        
+                    }
+                } //VStack
+                .padding(5.0)
+                .addBorder(Color.black,width: 1, cornerRadius: 10)
+                
+            } //End if controller.subCategoryIsEditing
+            //End of right side
+        }//End of HStack
+    }//End of body
 }
 
 struct SubCatList_Previews: PreviewProvider {
@@ -46,6 +116,8 @@ struct SubCatList_Previews: PreviewProvider {
     static var previews: some View {
         
         SubCatList()
+            .environmentObject(MainController())
+            .preferredColorScheme(.light)
         
     }
 }
