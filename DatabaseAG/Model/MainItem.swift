32dd5dae455d310eec900be032c85c5c99a437b3
badpolymer <roans_policy.07@icloud.com>
2022-07-24 00:46:21 +0800

    Remove MainCatRow.swift which is no longer used

--- DatabaseAG/Model/MainItem.swift
+++ DatabaseAG/Model/MainItem.swift
@@ -9,28 +9,30 @@ import Foundation
 import RealmSwift
 
 class MainItem : Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    
     var familyPN: String {
         var prefix : String {return (self.prefixes.isEmpty ? "" : "()") + (self.prefixPN ?? "")}
         var midfix : String {return self.midfixes.isEmpty ? "" : "()"}
         var mainfix : String {return self.mainfixes.isEmpty ? "" : "()"}
         var suffix: String {return (self.suffix ?? "") + (self.suffixes.isEmpty ? "" : "()")}
-        return prefix + midfix + self.mainPN + mainfix + suffix
+        return prefix + midfix + self.mainPN! + mainfix + suffix
     }
    
     var prefixes: [ModifiableSection] = []
-    var prefixPN: String?
+    @Persisted var prefixPN: String?
     var midfixes: [ModifiableSection] = []
-    var mainPN: String
+    @Persisted var mainPN: String?
     var mainfixes: [ModifiableSection] = []
-    var suffix: String?
+    @Persisted var suffix: String?
     var suffixes : [ModifiableSection] = []
     
-    init(mainPN: String, name:String) {
-        self.mainPN = mainPN
-        self.itemName = name
-    }
+//    init(mainPN: String, name:String) {
+//        self.mainPN = mainPN
+//        self.itemName = name
+//    }
     
-    var itemName : String
+    var itemName : String?
     
     
     @Persisted(originProperty: "items") var category : LinkingObjects<SubCategory>
